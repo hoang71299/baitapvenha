43b607b497128ce86697d4efad60ecceab3466eb
hoang huynh <huynhhoang712003@gmail.com>
2024-05-13 17:08:08 +0700

    b

--- src/components/Form/BaiTap/Register.jsx
+++ src/components/Form/BaiTap/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Error from "./Error";
-import { json } from "react-router-dom";
 function Register() {
   const arr = [
     {
